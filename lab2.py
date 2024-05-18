708d68c6b114a31ef812894e6337b47f57b0977f
manh <eguitarsml@gmail.com>
2024-05-18 11:52:27 +0700

    add get info : FLOPs and #param

--- lab2.py
+++ lab2.py
@@ -69,3 +69,33 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from matplotlib.colors import ListedColormap
 
+import torch
+from thop import profile
+
+def FLOPs_and_Params(model, img_size, len_clip, device):
+    # generate init video clip
+    model.to(device)
+    video_clip = torch.randn(1, 3, len_clip, img_size, img_size).to(device)
+
+    # set eval mode
+    model.trainable = False
+    model.eval()
+
+    flops, params = profile(model, inputs=(video_clip, ))
+
+    return flops, params
+    
+
+
+def get_info(config):
+    model = build_yolo2stream(config)
+
+    flops, params = FLOPs_and_Params(model, 224, 16, device='cuda')
+
+    print('==============================')
+    print('FLOPs : {:.2f} G'.format(flops / 1e9))
+    print('Params : {:.2f} M'.format(params / 1e6))
+    print('==============================')
+
+config = build_config()
+get_info(config)
--- utils/flops.py
+++ utils/flops.py
@@ -1,24 +1,31 @@
 import torch
 from thop import profile
+from model.TSN.YOLO2Stream import build_yolo2stream
 
 def FLOPs_and_Params(model, img_size, len_clip, device):
     # generate init video clip
+    model.to(device)
     video_clip = torch.randn(1, 3, len_clip, img_size, img_size).to(device)
 
     # set eval mode
     model.trainable = False
     model.eval()
 
-    print('==============================')
     flops, params = profile(model, inputs=(video_clip, ))
+
+    return flops, params
+    
+
+
+def get_info(config):
+    model = build_yolo2stream(config)
+
+    flops, params = FLOPs_and_Params(model, 224, 16, device='cuda')
+
     print('==============================')
     print('FLOPs : {:.2f} G'.format(flops / 1e9))
     print('Params : {:.2f} M'.format(params / 1e6))
-    
-    # set train mode.
-    model.trainable = True
-    model.train()
-
+    print('==============================')
 
 if __name__ == "__main__":
     pass
